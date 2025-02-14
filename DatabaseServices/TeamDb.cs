11b8f96d58a4a1ab6992a3a2cda3f877cc71f151
Richard Wilkinson <richardwilkinson1283@gmail.com>
2025-02-14 18:46:48 +0000

    Added folders and pages Added folders and pages for all the relevent models for creating items and saving to the database (excluding the logic).

---
+++ DatabaseServices/TeamDb.cs
@@ -0,0 +1,10 @@
+using ApprenticeEventManager.Models;
+using Microsoft.Data.Sqlite;
+
+namespace ApprenticeEventManager.DatabaseServices
+{
+  public class TeamDb
+  {
+    private static string connectionString = "Data Source=ApprenticeEventManager.db";
+  }
+}
